daad87635012a25c0df4496afcfdfcc18611c585
Petr Mervart <pmervart@gmail.com>
2019-03-05 23:34:39 +0100

    Revert "one line added"
    This reverts commit 1d3655a37498aefeb24493d91b8d0cde9f23206a.

--- GitHubTest01/Form1.cs
+++ GitHubTest01/Form1.cs
@@ -24,7 +24,6 @@ namespace GitHubTest01
         private void button2_Test2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Test 2");
-            MessageBox.Show("new test");
         }
     }
 }
